f9b90180b3b37f69a01d2f88ada6e6a61e5fe6fa
jlightyear <joseluis.estrach@gmail.com>
2015-05-06 18:42:09 +0200

    Added internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+ 
+  def set_locale
+    if cookies[:internalization_locale] && I18n.available_locales.include?(cookies[:internalization_locale].to_sym)
+      l = cookies[:internalization_locale].to_sym
+    else
+      l = I18n.default_locale
+      cookies.permanent[:internalization_locale] = l
+    end
+    I18n.locale = l
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'articles#index'
 
+  get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
