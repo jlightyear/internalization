50d06ac87553f81293352e3ae7db6ce4eb7bd9dd
jlightyear <joseluis.estrach@gmail.com>
2015-05-07 12:38:38 +0200

    Fixed language problem with cookie and other jquery problem

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,7 +2,7 @@ class SettingsController < ApplicationController
   def change_locale
     l = params[:locale].to_s.strip.to_sym
     l = I18n.default_locale unless I18n.available_locales.include?(l)
-    cookies.permanent[:educator_locale] = l
+    cookies.permanent[:internalization_locale] = l
     redirect_to request.referer || root_url
   end
 end
